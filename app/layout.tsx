034a7243900f3f6ade7af1d991295210784b0a7d
Divakar Yadav <60033014+divakar-yadav@users.noreply.github.com>
2026-01-03 06:23:23 -0600

    Remove manual head tags, rely on Next.js metadata API

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,11 +21,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/favicon.svg" />
-      </head>
       <body>
         <Header />
         <Navigation />
